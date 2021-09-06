e086db414b88d4a119c1e93f22954ddc54dc8e2d
Jess2D <jessicap_silveira@hotmail.com>
2021-09-07 07:02:15 +1200

    Add open weather

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-fetch('https://samples.openweathermap.org/data/2.5/weather?q=Lahore,PK&appid=339f059968fb5c38fee031595a383b42')
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=27498aefb60e29be93db8f28bc8e7bbd')
   .then(function(response) {
     console.log(response.json())
     return response.json();
