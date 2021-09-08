f0dddb9ba938189e77c369f2dae4e9e7b637e45d
Jess2D <jessicap_silveira@hotmail.com>
2021-09-09 08:59:35 +1200

    add fetch

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 
 fetch('http://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=27498aefb60e29be93db8f28bc8e7bbd')
-  .then(function(response) {
-    console.log(response.json())
-    return response.json();
-  })
-  .then(function(response) {
-     console.log(response.data)
-  });
+  .then(response  => response.json())
+  .then(response => console.log(response))
