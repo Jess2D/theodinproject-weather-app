a5aa4db9aa104e45a1bdfa34e3aec59c57a39fac
Jess2D <jessicap_silveira@hotmail.com>
2021-09-09 16:17:22 +1200

    add event listener to the form

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=27498aefb60e29be93db8f28bc8e7bbd')
-  .then(response  => response.json())
-  .then(response => console.log(response))
+let form = document.getElementById("form");
+
+
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+  let search = form.elements["search"].value
+  fetch('http://api.openweathermap.org/data/2.5/weather?q='+search+'&appid=27498aefb60e29be93db8f28bc8e7bbd')
+    .then(response  => response.json())
+    .then(response => console.log(response))
+
+})
+
+
