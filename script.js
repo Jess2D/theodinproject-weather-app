bd836226acea763b97a3862078d1108382f18bce
Jess2D <jessicap_silveira@hotmail.com>
2021-09-12 21:14:09 +1200

    Add temperature function

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
 
 let form = document.getElementById("form");
-let dataName, dataTemperature;
+let dataName, dataFellsLike, dataTemperature, dataTemperatureMax, dataTemperatureMin, dataWeather, dataWeatherIcon, dataSunrise, dataSunset;
+let dataChanceRain, dataHumidity, dataWind, dataPrecipitation, dataUvIndex
 
 form.addEventListener("submit", function(event){
   event.preventDefault();
   let search = form.elements["search"].value
-  fetch('http://api.openweathermap.org/data/2.5/weather?q='+ search +'&appid=27498aefb60e29be93db8f28bc8e7bbd')
+  fetch('http://api.openweathermap.org/data/2.5/weather?q='+ search +'&appid=27498aefb60e29be93db8f28bc8e7bbd&units=metric')
     .then(response  => response.json())
     .then(response => {
       console.log(response);
        dataName = response.name
        dataTemperature = {...response.main}
-       console.log(dataTemperature)
-       console.log(dataTemperature["fells_like"])
-       viewName(dataName)
-       temperature(dataTemperature)
+       console.log(response.main["temp"])
+       ViewName(dataName)
+       Temperature(dataTemperature)
+       FellsLike(dataTemperature)
+     
     })
 
 
 
 });
 
-function viewName(name){
+function ViewName(name){
   const locationName = name
   const nameID = document.getElementById("name")
   nameID.innerHTML = locationName
 }
 
-function temperature(temperature){
-  const fells_Like = temperature["feels_like"]
+function Temperature(tempt){
+  const temp = tempt["temp"]
   const temperatureID = document.getElementById("temperature")
-  const C = getCelsius(fells_Like)
-  temperatureID.innerHTML = "Fells like: "+ C
+  temperatureID.innerHTML = "Temp: "+temp
 }
 
-function getCelsius(temp){
-    const c = (temp-32)*0.555555556
-    return c;
+function FellsLike(temperature){
+  const fells_Like = temperature["feels_like"]
+  const fellsLikeID = document.getElementById("fellsLike")
+  fellsLikeID.innerHTML = "Fells like: "+ Math.round(fells_Like)
 }
+
+function temperature(temperature){
+
+}
+
 
 
