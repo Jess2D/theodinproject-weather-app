bc79d7552154dfb86425077321d3a46ac8653c7d
Jess2D <jessicap_silveira@hotmail.com>
2021-09-22 20:24:01 +1200

    Add https to links

--- src/wind.js
+++ src/wind.js
@@ -5,7 +5,7 @@ function WindSpeed(wind) {
   const speed = wind.speed;
   const windSpeedID = document.getElementById("windSpeed");
   console.log(speed);
-  windSpeedID.innerHTML = "Wind: " + (speed * 3.6).toFixed(2) + "km/h";
+  windSpeedID.innerHTML = "Wind: " + speed + "meter/sec";
 }
 
 /* @description Represents a Wind direction, degrees (meteorological) Function
