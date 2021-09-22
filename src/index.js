1f08ead409c78e6c2c3505da1f2662f004285a66
Jess2D <jessicap_silveira@hotmail.com>
2021-09-22 20:17:42 +1200

    Build my project

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ form.addEventListener("submit", function (event) {
   let search = form.elements["search"].value;
   fetch(
     "https://api.openweathermap.org/data/2.5/weather?q=" +
-      search +
+      "london" +
       "&appid=27498aefb60e29be93db8f28bc8e7bbd&units=metric"
   )
     .then((response) => response.json())
